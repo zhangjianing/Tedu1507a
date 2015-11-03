48c6d3a8246811db6856bc92533f7a88d117ff9f
jiyingxin <736907613@qq.com>
2015-11-03 21:48:39 +0800

    添加了两个注释。 大声告诉我，还有谁在写代码？？

--- SourceCode/玩/BaseProject/NetManager/BaseNetManager.m
+++ SourceCode/玩/BaseProject/NetManager/BaseNetManager.m
@@ -34,7 +34,7 @@ static AFHTTPSessionManager *manager = nil;
     NSInteger count = keys.count;
 /*  习惯
  for(int i = 0; i < params.allKeys.count; i ++)
- 假设for循环循环1000次，params.allKeys实际上调用的[params allKeys], 会调用allKeys1000次。 OC语言特性是runtime，实际上我们调用一个方法，底层操作是 在一个方法列表中搜索你调用的方法所在的地址，然后调用完毕之后把这个方法名转移到常用方法列表。所以如果再次执行某个方法就在常用方法列表中搜索调用，效率更高。
+ 假设for循环循环1000次，params.allKeys实际上调用的[params allKeys], 会调用allKeys1000次。 OC语言特性是runtime，实际上我们调用一个方法，底层操作是有两个列有方法的列表，常用表和总列表。CPU先在常用表中搜索调用的方法指针，如果找不到，再到总列表中搜索。 在总列表中搜索你调用的方法所在的地址，然后调用完毕之后把这个方法名转移到常用方法列表。如果再次执行某个方法就在常用方法列表中搜索调用，效率更高。但是毕竟每次搜索方法都是耗时的，而swift语言则没有运行时，即没有这个搜索过程。 这是swift比oc效率高20%的原因。 为了避免搜索耗时，我们在for循环外部就把调用次数算出来，这样每次for循环只需要去count所在地址读变量值即可。 这样for循环效率更高。
  */
     for (int i = 0; i < count; i++) {
         if (i == 0) {
