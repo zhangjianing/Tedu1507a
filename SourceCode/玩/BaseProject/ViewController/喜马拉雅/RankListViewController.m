841a5f7072d1f7564c4186405718b16fabe90e95
jiyingxin <736907613@qq.com>
2015-11-09 17:02:53 +0800

    听 啊坤诚呐 同学的教诲，修改了行高设置方式。   把tableView的头部 脚步 刷新代码操作 放到懒加载中写， 让代码结构更紧凑

--- SourceCode/玩/BaseProject/ViewController/喜马拉雅/RankListViewController.m
+++ SourceCode/玩/BaseProject/ViewController/喜马拉雅/RankListViewController.m
@@ -27,10 +27,43 @@
         _tableView=[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
         _tableView.delegate = self;
         _tableView.dataSource = self;
+/*啊坤诚呐 同学说了 行高一样用rowheight 不然影响效率啊
+    所以这里删除了返回高度的协议方法，改为rowHeight
+ */
+        _tableView.rowHeight = 170/2;
+        _tableView.estimatedRowHeight = 170/2;
+        
         [_tableView registerClass:[XiMaCategoryCell class] forCellReuseIdentifier:@"Cell"];
         [self.view addSubview:_tableView];
         [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.edges.mas_equalTo(0);
+        }];
+        self.tableView.header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+            [self.ximaVM refreshDataCompletionHandle:^(NSError *error) {
+                if (error) {
+                    [self showErrorMsg:error];
+                }else{
+                    [self.tableView reloadData];
+                }
+                //   重置脚步 没有更多数据
+                [_tableView.footer resetNoMoreData];
+                [_tableView.header endRefreshing];
+            }];
+        }];
+        self.tableView.footer=[MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
+            [self.ximaVM getMoreDataCompletionHandle:^(NSError *error) {
+                if (error) {
+                    [self showErrorMsg:error.localizedDescription];
+                    if (error.code == 999) {
+                        [self.tableView.footer endRefreshingWithNoMoreData];
+                    }else{
+                        [_tableView.footer endRefreshing];
+                    }
+                }else{
+                    [self.tableView reloadData];
+                    [_tableView.footer endRefreshing];
+                }
+            }];
         }];
     }
     return _tableView;
@@ -51,34 +84,6 @@
     // Do any additional setup after loading the view.
     [Factory addMenuItemToVC:self];
     self.title = @"音乐TOP50";
-    
-    self.tableView.header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
-        [self.ximaVM refreshDataCompletionHandle:^(NSError *error) {
-            if (error) {
-                [self showErrorMsg:error];
-            }else{
-                [self.tableView reloadData];
-            }
-//   重置脚步 没有更多数据
-            [_tableView.footer resetNoMoreData];
-            [_tableView.header endRefreshing];
-        }];
-    }];
-    self.tableView.footer=[MJRefreshAutoNormalFooter footerWithRefreshingBlock:^{
-        [self.ximaVM getMoreDataCompletionHandle:^(NSError *error) {
-            if (error) {
-                [self showErrorMsg:error.localizedDescription];
-                if (error.code == 999) {
-                    [self.tableView.footer endRefreshingWithNoMoreData];
-                }else{
-                    [_tableView.footer endRefreshing];
-                }
-            }else{
-                [self.tableView reloadData];
-                [_tableView.footer endRefreshing];
-            }
-        }];
-    }];
     [self.tableView.header beginRefreshing];
 }
 
@@ -89,7 +94,6 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     XiMaCategoryCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     cell.titleLb.text = [self.ximaVM titleForRow:indexPath.row];
-
     [cell.iconIV.imageView setImageWithURL:[self.ximaVM iconURLForRow:indexPath.row] placeholderImage:[UIImage imageNamed:@"cell_bg_noData_1"]];
     cell.orderLb.text = @(indexPath.row + 1).stringValue;
     cell.descLb.text = [self.ximaVM descForRow:indexPath.row];
@@ -98,9 +102,6 @@
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-}
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 170/2;
 }
 
 - (void)didReceiveMemoryWarning {
